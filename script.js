4c6c5667f07b4bef9f32c9bd523216ad6b71237e
Martin-Juncos <prof.mcjuncos@gmail.com>
2024-11-26 16:35:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  const failure = document.getElementById("failure");
+  // Puedes agregar alguna lógica en JS si necesitas animación adicional o efectos.
+  console.log("Page loaded, failure running...");
+};
